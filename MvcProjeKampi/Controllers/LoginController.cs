b9863230b0a4a75190bbe786a13818deb5ada250
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-12-25 20:36:58 +0300

    Add project files.

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -1,40 +1,43 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcProjeKampi.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        [HttpGet]
+        AdminManager adminmanager = new AdminManager(new EfAdminDal());
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(Admin p)
+        public ActionResult Index(Admin admin)
         {
-            Context c=new Context();
-            var values = c.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
 
-            if (values != null)
+            var adminuserinfo = adminmanager.GetAdmin(admin.AdminUserName, admin.AdminPassword);
+
+            if (adminuserinfo != null)
             {
-               return RedirectToAction("Index", "Gallery");
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName, false);
+                Session["AdminUserName"] = adminuserinfo.AdminUserName;
+                return RedirectToAction("Index", "AdminCategory");
             }
             else
             {
-                return RedirectToAction("Index");
+                return View();
             }
-
-            return View();
-
         }
     }
 }
